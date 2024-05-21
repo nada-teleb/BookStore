8f5d148af80cd04c45b26f739887de14cf3506f5
nada_teleb <telebnada@gmail.com>
2024-05-21 18:14:06 +0300

    Create a new model and corresponding route for creating a book.

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,38 @@
 import express from "express";
 import mongoose from "mongoose";
-import { PORT, mongodbURL} from "./config.js";
+import { PORT, mongodbURL } from "./config.js";
+import { Book } from "./Models/bookModel.js";
 
 const app = express();
 
+// Middleware for parsing request body
+app.use(express.json());
 
 app.get('/', (req, res) => {
 	console.log(req);
 	return res.status(234).send('Welcome to MERN tutorial!');
+});
+
+// Route to save a new book
+app.post('/books', async (req, res) => {
+	try {
+		if (!req.body.title || !req.body.author || !req.body.publishYear)
+			return res.status(400).send({ message: 'Send all required fields: title, author, publishYear' });
+
+		const newBook = {
+			title: req.body.title,
+			author: req.body.author,
+			publishYear: req.body.publishYear
+		};
+
+		const book = await Book.create(newBook);
+
+		return res.status(201).send(book);
+
+	} catch (error) {
+		console.log(error);
+		res.status(500).send({ message: error.message });
+	}
 });
 
 
@@ -18,6 +43,6 @@ mongoose.connect(mongodbURL)
 			console.log(`App is listening on ${PORT}`);
 		});
 	})
-	.catch(err => {
-		console.log(err);
+	.catch(error => {
+		console.log(error);
 	});
