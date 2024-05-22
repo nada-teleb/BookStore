ad758593c5bb85e40156d44660fc6400ee471089
nada_teleb <telebnada@gmail.com>
2024-05-22 18:09:20 +0300

    Adding CRUD pages as components for SPA, and define corresponding routes.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import CreateBook from './Pages/CreateBook';
+import ShowBook from './Pages/ShowBook';
+import EditBook from './Pages/EditBook';
+import DeleteBook from './Pages/DeleteBook';
 
 const App = () => {
 	return (
-		<div className='bg-red-400 text-white'>
-			App
-		</div>
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/books/create" element={<CreateBook />} />
+			<Route path="/books/details/:id" element={<ShowBook />} />
+			<Route path="/books/edit/:id" element={<EditBook />} />
+			<Route path="/books/delete/:id" element={<DeleteBook />} />
+		</Routes>
 	)
 }
 
