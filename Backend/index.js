7f6cce7cccc9650d53db48578035053ebcf43fd0
nada_teleb <telebnada@gmail.com>
2024-05-22 15:19:40 +0300

    Adding routed for basic CRUD operations on Book module.

--- Backend/index.js
+++ Backend/index.js
@@ -35,7 +35,6 @@ app.post('/books', async (req, res) => {
 	}
 });
 
-
 // Route to get ALL books from the database
 app.get('/books', async (req, res) => {
 	try {
@@ -45,6 +44,54 @@ app.get('/books', async (req, res) => {
 			count: books.length,
 			data: books
 		});
+
+	} catch (error) {
+		console.log(error);
+		res.status(500).send({ message: error.message });
+	}
+});
+
+// Route to get one book from the database by ID
+app.get('/books/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const book = await Book.findById(id);
+		return res.status(200).send(book);
+
+	} catch (error) {
+		console.log(error);
+		res.status(500).send({ message: error.message });
+	}
+});
+
+// Route to update a book
+app.put('/books/:id', async (req, res) => {
+	try {
+		if (!req.body.title || !req.body.author || !req.body.publishYear)
+			return res.status(400).send({ message: 'Send all required fields: title, author, publishYear' });
+
+		const { id } = req.params;
+		const result = await Book.findByIdAndUpdate(id, req.body);
+
+		if (!result)
+			return res.status(404).json({ message: 'Book not found' });
+		return res.status(200).send({ message: 'Book updated successfully' });
+
+	} catch (error) {
+		console.log(error);
+		res.status(500).send({ message: error.message });
+	}
+});
+
+// Route to dalete a book
+app.delete('/books/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const result = await Book.findByIdAndDelete(id);
+
+		if (!result)
+			return res.status(404).json({ message: 'Book not deleted' });
+		return res.status(200).send({ message: 'Book deleted successfully' });
 
 	} catch (error) {
 		console.log(error);
