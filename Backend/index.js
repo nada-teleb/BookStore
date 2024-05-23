2460925a17cca23e4811987755dc66586b701f8b
nada_teleb <telebnada@gmail.com>
2024-05-23 18:10:50 +0300

    Add Home page for books list 'fetched from database' with CRUD operations routes 'links'.

--- Backend/index.js
+++ Backend/index.js
@@ -11,13 +11,13 @@ app.use(express.json());
 
 // Middleware for handling CORS policy
 // Option 1: Allow all origins with default of cors (*)
-// app.use(cors());
+app.use(cors());
 // Option 2: Allow custom origins
-app.use(cors({
-	origin: 'http://localhost:3000',
-	methods: ['GET', 'POST', 'PUT', 'DELETE'],
-	allowedHeaders: ['Content-Type']
-}));
+// app.use(cors({
+// 	origin: 'http://localhost:3000',
+// 	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// 	allowedHeaders: ['Content-Type']
+// }));
 
 
 
--- Frontend/src/Pages/ShowBook.jsx
+++ Frontend/src/Pages/ShowBook.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ShowBook = () => {
 	return (
 		<div>
-
+			ShowBook
 		</div>
 	)
 }
